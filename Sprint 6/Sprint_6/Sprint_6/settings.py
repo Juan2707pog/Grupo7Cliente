c5203b5886873d0a767964e3a10356f97bfce233
carlos-ada <carlosm.villarada@gmail.com>
2021-02-18 17:56:10 +0100

    Arreglo problema git

--- Sprint 6/Sprint_6/Sprint_6/settings.py
+++ Sprint 6/Sprint_6/Sprint_6/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat', 'config', 'feed',
+    'header', 'info1', 'login',
+    'post', 'register',
+
 ]
 
 MIDDLEWARE = [
